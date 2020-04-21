f0d720fb4d792ee6b393b62891066c5cc92f0765
Stanko Culaja <culaja@gmail.com>
2020-04-21 18:46:24 +0200

    Added skeleton for planning poker.

---
+++ Backend/CommandSide/WebApplication/Controllers/SessionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class SessionController : ControllerBase
+    {
+    }
+}
---
+++ Backend/Shared/DomainEvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Shared
+{
+    public static class DomainEvents
+    {
+        public sealed class SessionCreated
+        {
+            public Guid SessionId { get; }
+
+            public SessionCreated(Guid sessionId)
+            {
+                SessionId = sessionId;
+            }
+        }
+
+        public sealed class MemberVoted
+        {
+            public Guid SessionId { get; }
+            public string Member { get; }
+
+            public MemberVoted(Guid sessionId, string member)
+            {
+                SessionId = sessionId;
+                Member = member;
+            }
+        }
+
+        public sealed class SessionCleared
+        {
+            public Guid SessionId { get; }
+
+            public SessionCleared(Guid sessionId)
+            {
+                SessionId = sessionId;
+            }
+        }
+    }
+}
